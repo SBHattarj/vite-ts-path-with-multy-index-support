4346d02bb8fcaa622976ba8b0a7392d41a9f0fa9
SBHattarj <subhambhsubhambh@gmail.com>
2022-07-02 15:27:14 +0000

    finnished package

---
+++ Readme.md
@@ -0,0 +1,40 @@
+# Vite Ts Path With Multy Index Support
+
+This npm package was creaed for my personal use, so a lot more testing is required. It is only tested on linux.
+
+If you still wanna use this package here is a description of how it works.
+
+you can install this as bellow:
+```properties
+npm i vite-ts-path-with-multy-index-support
+```
+
+you can use this as follows:
+```js
+import { defineConfig } from 'vite'
+import viteTspathWithMultyIndexSuport from "./plugin/plugin"
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+  plugins: [viteTspathWithMultyIndexSuport()]
+})
+
+```
+It only exports one function as seen here.
+## Ittakes one optional object as it's argument. It has the given attributes:
+- **root**: ```string``` to set the root of the project. by default it would be the root set in vite config, if not set then it uses baseUrl of tsConfig, if not set uses ```process.cwd()```. It is used to resolve bare import from root when module resolution is classic in ts.
+- **exclude**: ```string[]```  exclude certain modules from being resolved by this plugin. It by default will also exclude certain modules added at vite config.
+- **extensions**: ```string[]``` extensions to look for when doing root import. It will look for the import and get the first match in the same order as given.it is not used if extensions is defined within resolve of vite config. This defaults to:
+```js
+[
+    '.mjs',
+    '.js',
+    '.ts',
+    '.jsx',
+    '.tsx',
+    '.json'
+]
+```
+- **moduleResolution**: ```"classic" | "node"``` same as the compilerOption of  the same name for tsconfig. It is only used to check if bare root import should be done.
+
+If you would like to you can help me out with windows integration, better documentation and project structure.
---
+++ plugin/plugin.ts
@@ -0,0 +1,133 @@
+import {getTsconfig} from "get-tsconfig"
+import { UserConfig } from "vite"
+import { resolveOne } from "npm-module-path"
+import path from "path"
+import fs from "fs-extra"
+import { IPackageJson } from "package-json-type"
+import { normalizePath } from "vite"
+import fg from "fast-glob"
+
+const resolveMainFromPackage = (config: IPackageJson, ssr?: boolean): string => {
+    if(ssr) config.module ?? config.main ?? "./index.js"
+    if(config.browser == null) return config.module ?? config.main ?? "./index.js"
+    if(typeof config.browser == "string") return config.browser ?? "./index.js"
+    return config.browser?.[config.module ?? config.main ?? "index.js"] 
+        ?? config.browser?.[`./${config.module ?? config.main ?? "index.js"}`]
+        ?? "./index.js"
+}
+
+const resolveModuleDir = async (module: string, importer: string, root: string = process.cwd()) => {
+    let importDir = path.resolve(root, importer.replace(/\/[^\/]+$/, ""))
+    let resolvedDir: void | string
+    const [_, mainModule, subModule] = [...(module.match(/([^\/]+)(.*)/) ?? [])]
+    while(resolvedDir == null) {
+        resolvedDir = await resolveOne(mainModule, importDir)
+        if(importDir === root) return `${resolvedDir}${subModule}`
+        importDir = path.resolve(importDir, "..")
+    }
+    return `${resolvedDir}${subModule}`
+}
+
+const getPackageJson = async (
+    module: string, 
+    importer: string, 
+    root: string = process.cwd()
+): Promise<void | {moduleDir: string, packageJson: IPackageJson}> => {
+    const moduleDir = await resolveModuleDir(module, importer, root)
+    if(typeof moduleDir !== "string") return
+    try {
+        return {
+            moduleDir,
+            packageJson: await fs.readJSON(path.resolve(moduleDir, "./package.json"))
+        }
+    } catch {}
+}
+
+const hasMultyIndex = async (module: string, root: string = process.cwd()) => {
+    const packageData = await getPackageJson(module, `${process.cwd()}/index.json`, root)
+    const packageJson = packageData?.packageJson
+    return packageJson?.browser != null
+
+}
+
+const resolveRootBareImport = async (id: string, root: string, allowedExtensions: string[]) => {
+    const fullPath = normalizePath(path.resolve(root, id))
+    const posibleImports = await fg(`${fullPath}@(${allowedExtensions.join("|")})`)
+    for(const extension of allowedExtensions) {
+        const matchedImport = posibleImports.find( Import => Import.endsWith(extension));
+        if(matchedImport != null) return matchedImport
+    }
+}
+
+export default function viteTspathWithMultyIndexSupport(
+    {
+        exclude = [], 
+        extensions = [
+            '.mjs',
+            '.js',
+            '.ts',
+            '.jsx',
+            '.tsx',
+            '.json'
+        ],
+        moduleResolution,
+        ...pluginOptions
+    }: {root?: string, exclude?: string[], extensions?: string[], moduleResolution?: "classic" | "node" } = {}
+): {
+    name: string,
+    enforce: "pre",
+    resolveId: (path: string, importer?: string, options?: {ssr?: boolean}) => Promise<string | void>,
+    config: (config: UserConfig) => Promise<UserConfig>
+} {
+    const relativeAbsolutePrefixes = ["./", "../", "/"]
+    const tsConfig = getTsconfig()?.config
+    let viteConfig: UserConfig = {}
+    const packageJson = fs.readJsonSync(path.resolve(process.cwd(), "./package.json"))
+    const allDependencies = [...Object.keys(packageJson.dependencies ?? {}), ...Object.keys(packageJson.devDependencies)]
+    const moduleWithMultyIndex: string[] = []
+
+    return {
+        name: "vite-ts-path-with-multy-index-support",
+        enforce: "pre",
+        async config(config) {
+            viteConfig = config
+            for(const module of allDependencies) {
+                if(
+                    config.resolve?.dedupe?.includes(module) 
+                    || config.optimizeDeps?.include?.includes(module)
+                    || exclude.includes(module)
+                ) continue
+                if(await hasMultyIndex(module)) moduleWithMultyIndex.push(module)
+            }
+            config.optimizeDeps = {
+                ...config.optimizeDeps,
+                exclude: [
+                    ...(config.optimizeDeps?.exclude ?? []),
+                    ...moduleWithMultyIndex
+                ]
+            }
+            return config
+        },
+        async resolveId(id, importer, options) {
+            try {
+                if(importer == null) return
+                const root = path.resolve(pluginOptions.root ?? viteConfig.root ?? tsConfig?.compilerOptions?.baseUrl ?? "./")
+                if(
+                    relativeAbsolutePrefixes.some(
+                        prefix => id.startsWith(prefix) 
+                        || prefix.substr(0, (id.length - 1) || 1) === id
+                    )
+                ) return
+                const allowedExtensions = viteConfig?.resolve?.extensions ?? extensions
+
+                const packageInfo = await getPackageJson(id, importer)
+                if(packageInfo == null) return (moduleResolution ?? tsConfig?.compilerOptions?.moduleResolution) === "classic" ?  await resolveRootBareImport(id, root, allowedExtensions) : null
+                const main = resolveMainFromPackage(packageInfo.packageJson, options?.ssr)
+                const resolvedId = path.resolve(packageInfo.moduleDir, main)
+                const doesModuleExists = await fs.pathExists(resolvedId)
+                if(!doesModuleExists) return
+                return resolvedId
+            } catch {}
+        },
+    }
+}
